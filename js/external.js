79e63472a350199936cb1de57380f199f1da3098
Dereck Rascon <dereckrascon76@gmail.com>
2022-10-06 09:08:34 -0700

    wolring on exercise

---
+++ js/external.js
@@ -0,0 +1,11 @@
+//"use strict";
+
+//console.log('Hello from external JavaScript!');
+
+//var userInput = prompt('What is your favorite color?:');
+//console.log('The user entered: ' + userInput);
+
+//alert("Cool my favorite color is "+ userInput +" too!.");
+
+var userInput = prompt('How many days would you like to rent The Little Mermaid?:');
+console.log('The user entered: ' + userInput);
