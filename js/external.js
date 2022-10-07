4ec0f8ae24dc734f22b3c8326b482b6bfa2cd171
Dereck Rascon <dereckrascon76@gmail.com>
2022-10-06 21:16:30 -0700

    updated my code to work a little better

--- js/external.js
+++ js/external.js
@@ -1,56 +1,61 @@
-"use strict";
-
+ "use strict";
+//
 console.log('Hello from external JavaScript!');
 
-alert("Welcome to My Website!")
+alert("Welcome to My Website!");
 
-var userInput = prompt('What is your favorite color?:');
-console.log('The user entered: ' + userInput);
+let inputUser = prompt('What is your favorite color?');
+console.log('The user entered: ' + inputUser);
 
-alert("Cool my favorite color is "+ userInput +" too!.");
-
+alert("Cool my favorite color is "+ inputUser +" too!");
+//
 // The following is #3 part 1
-var userInput = prompt('How many days would you like to rent The Little Mermaid?:');
-console.log('The user entered: ' + userInput);
+let pricePerDay = prompt("How much does it cost to rent a movie?");
+let littleMermaid = prompt("How many days would you like to rent The Little Mermaid?");
+console.log("The user entered: " + littleMermaid);
+let brotherBear = prompt("How many days would you like to rent Brother Bear?");
+console.log("The user entered: " + brotherBear);
+let hercules = prompt("How many days would you like to rent Hercules?");
+console.log("The user entered: " + hercules);
 
+alert("This transaction will be $" + ((littleMermaid * pricePerDay) + (brotherBear * pricePerDay) + (hercules * pricePerDay)));
+//
+// //
+// // //the following is for #3 part 2
+let googleHours = parseFloat (prompt ("How many hours did you work for Google?"));
+let payGoogle = parseFloat (prompt ("How much does Google pay you an hour?"));
+let amazonHours = parseFloat (prompt ("How many hours did you work for Amazon?"));
+let payAmazon = parseFloat (prompt ("How much does Amazon pay you an hour?"));
+let faceBookHours = parseFloat (prompt ("How many hours did you work for Facebook?"));
+let payFaceBook = parseFloat (prompt("How much does Facebook pay you an hour?"));
 
-var userInput = prompt('How many days would you like to rent Brother Bear?:');
-console.log('The user entered: ' + userInput);
+alert("Your total pay is $" + ((payGoogle * googleHours) + (payAmazon * amazonHours) + (payFaceBook * faceBookHours)));
+//
+//This state below is for me to see how i did it first and work around it so the code can work for the statement above.
+// let userInput = prompt('How many hours did you work for Google?:');
+// console.log('The user entered: ' + userInput);
+//
+// let userInput = prompt('How many hours did you work for Amazon?:');
+// console.log('The user entered: ' + userInput);
+//
+// let userInput = prompt('How many hours did you work for Facebook?:');
+// console.log('The user entered: ' + userInput);
+//
+//let payGoogle = 400 * (userInput);
+//
+// let payAmazon = 380 * (userInput);
+//
+//let payFaceBook = 350 * (userInput);
+//
+//alert("The total amount made is ("+payGoogle+", "+payAmazon+", "+payFaceBook+") dollars.");
+//
+//The following is for #3 part 3
+//
+let classNotFull = confirm("Is the class Coding101 full? (Ok = Yes / Cancel = No)");
 
-var userInput = prompt('How many days would you like to rent Hercules?:');
-console.log('The user entered: ' + userInput);
+let classConflict = confirm("Does the class conflict with your schedule? (Ok = Yes / Cancel = No)");
 
-var total = (userInput * 3 )
-
-alert("This transaction will be "+ total +" dollars");
-
-
-//the following is for #3 part 2
-
-var userInput = prompt('How many hours did you work for Google?:');
-console.log('The user entered: ' + userInput);
-
-var userInput = prompt('How many hours did you work for Amazon?:');
-console.log('The user entered: ' + userInput);
-
-var userInput = prompt('How many hours did you work for Facebook?:');
-console.log('The user entered: ' + userInput);
-
-var payGoogle = 400 * (userInput);
-
-var payAmazon = 380 * (userInput);
-
-var payFaceBook = 350 * (userInput);
-
-alert("The total amount made is ("+payGoogle+", "+payAmazon+", "+payFaceBook+") dollars.");
-
-// The following is for #3 part 3
-
-var classNotFull = confirm("is the class full? (Ok = Yes / Cancel = No)");
-
-var classConflict = confirm("Does the class conflict with your schedule? (Ok = Yes / Cancel = No)");
-
-if (classConflict == true  || classNotFull == true) {
+if (classConflict === true  || classNotFull === true) {
     alert("You cannot register for this class.");
 }else {
     alert("You can register for this class.");
