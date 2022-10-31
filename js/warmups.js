697c531ff66cc42ebec1cf08bac6c066ae7b1b42
Dereck Rascon <dereckrascon76@gmail.com>
2022-10-31 07:45:06 -0700

    finsihed warmup exercise

--- js/warmups.js
+++ js/warmups.js
@@ -39,53 +39,70 @@
 // TODO
 //Write a function that takes in an array of objects and returns the object with the largest height property.
 
-
-
-const hamsters = [
-    {
-        name: "Hamtaro",
-        heightInMM: 86,
-        fur: ['orange', 'white'],
-        gender: "male",
-        dateOfBirth: "August 6"
-    } , {
-        name: "Bijou",
-        heightInMM: 75,
-        fur: ['white'],
-        gender: "female",
-        dateOfBirth: "July 10"
-    } , {
-        name: "Oxnard",
-        heightInMM: 100,
-        fur: ['grey', 'white'],
-        gender: "male",
-        dateOfBirth: "May 3"
-    } , {
-        name: "Boss",
-        heightInMM: 120,
-        fur: ['brown', 'white'],
-        gender: "male",
-        dateOfBirth: "September 21"
-    } , {
-        name: "Snoozer",
-        heightInMM: 85,
-        fur: ['brown', 'white', "pink"],
-        gender: "male",
-        dateOfBirth: "January 14"
-    }
-];
 //
-// function height(hamster) {
-//     let tallest = {heightInMM: 0};
-//     hamster.forEach(function (hamsters) {
-//         if (hamsters.heightInMM > tallest.heightInMM) {
-//             tallest = hamsters;
-//         }
-//     });
-//     return tallest;
+//
+// const hamsters = [
+//     {
+//         name: "Hamtaro",
+//         heightInMM: 86,
+//         fur: ['orange', 'white'],
+//         gender: "male",
+//         dateOfBirth: "August 6"
+//     } , {
+//         name: "Bijou",
+//         heightInMM: 75,
+//         fur: ['white'],
+//         gender: "female",
+//         dateOfBirth: "July 10"
+//     } , {
+//         name: "Oxnard",
+//         heightInMM: 100,
+//         fur: ['grey', 'white'],
+//         gender: "male",
+//         dateOfBirth: "May 3"
+//     } , {
+//         name: "Boss",
+//         heightInMM: 120,
+//         fur: ['brown', 'white'],
+//         gender: "male",
+//         dateOfBirth: "September 21"
+//     } , {
+//         name: "Snoozer",
+//         heightInMM: 85,
+//         fur: ['brown', 'white', "pink"],
+//         gender: "male",
+//         dateOfBirth: "January 14"
+//     }
+// ];
+// //
+// // function height(hamster) {
+// //     let tallest = {heightInMM: 0};
+// //     hamster.forEach(function (hamsters) {
+// //         if (hamsters.heightInMM > tallest.heightInMM) {
+// //             tallest = hamsters;
+// //         }
+// //     });
+// //     return tallest;
+// // }
+// // console.log(height(hamsters));
+//
+// function largestHeight(arr){
+//
 // }
-// console.log(height(hamsters));
 
-function largestHeight(arr){
-
+// TODO
+//Write a function called fizzBuzz from 1-100.
+function fizzBuzz(){
+    for(let i = 1; i < 100; i++){
+        if(i % 15 === 0){
+            console.log("FizzBuzz");
+        } else if(i % 3 === 0) {
+            console.log("Fizz");
+        } else if(i % 5 === 0) {
+            console.log("Buzz");
+        } else{
+            console.log(i);
+        }
+    }
 }
+fizzBuzz();
