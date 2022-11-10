d88e375d3f457f59ab60f543d845d918d9d90a41
Dereck Rascon <dereckrascon76@gmail.com>
2022-11-09 18:29:00 -0700

    completed defuse-the-bom

--- js/warmups.js
+++ js/warmups.js
@@ -62,7 +62,7 @@
 // ];
 // //
 // // function height(hamster) {
-// //     let tallest = {heightInMM: 0};
+//     let tallest = {heightInMM: 0};
 // //     hamster.forEach(function (hamsters) {
 // //         if (hamsters.heightInMM > tallest.heightInMM) {
 // //             tallest = hamsters;
@@ -143,44 +143,136 @@
 
 // TODO Write a function that takes in two string arguments: the first being the string assessed, and the second being a single letter. The function should return a count of the occurances of the letter (second argument) within the string (first argument). This function should count both upper and lowercase instances of the letter
 
-function charCount(str, letter){
-    let letterCount= 0;
-    for(let position = 0; position < str.length; position++){
-        if(str.charAt(position) === letter){
-           letterCount += 1;
-        }
-    }
-    return letterCount;
-}
-
-console.log(charCount("Mississippi", "s"))
+// function charCount(str, letter){
+//     let letterCount= 0;
+//     for(let position = 0; position < str.length; position++){
+//         if(str.charAt(position) === letter){
+//            letterCount += 1;
+//         }
+//     }
+//     return letterCount;
+// }
+//
+// console.log(charCount("Mississippi", "s"))
 
 //https://www.w3resource.com/javascript-exercises/javascript-function-exercise-22.php
 
 //TODO Write a function that takes in an array of grocery item objects and returns the object with the highest weightInGrams property.
+//
+// const cart = [
+//     {
+//         name: "Kingsford Charcoal",
+//         weightInGrams: 4535
+//     }, {
+//         name: "Krazy Glue",
+//         weightInGrams: 5
+//     }, {
+//         name: "NVIDIA RTX 4090",
+//         weightInGrams: 2186
+//     } ];
+//
+//
+// function groceryItem(array){
+//     let highest = {weightInGrams: 0};
+//     for (let i = 0; i < array.length; i++){
+//         if(array[i].weightInGrams > highest.weightInGrams){
+//             highest = array[i]
+//         }
+//     }
+//     return highest;
+// }
+//
+// console.log(groceryItem(cart))
 
-const cart = [
+
+// TODO
+// Write a function that takes in an array of letters, and returns the array sorted in reverse alphabetical order.
+
+// const sortLetters =(["a", "b", "c", "d"])
+//
+// function arrayOfLetter(array){
+//     return array.reverse()
+// }
+//
+// console.log(arrayOfLetter(sortLetters))
+
+//TODO
+//2. Write a function that takes in a number and returns an object with the following properties.
+// 	1.  `value` which is to be set to the number passed into the function.
+// 	2. `doubled` which is to be set to the number passed into the function multiplied by 2.
+// 	3. `sign` which is to be set to a string, either `"positive"`, `"negative"`, or `"zero"`, corresponding to the value of the number passed into the function being either positive, negative, or 0
+
+
+
+//TODO
+// ex.
+// arrayOfWords("Lovely Day For Coding") // returns ["Lovely", "Day", "For", "Coding"]
+// arrayOfWords("One-eyed One-horned Flying Purple People Eater") // returns ["One-eyed", "One-horned", "Flying", "Purple", "People", "Eater"]
+// arrayOfWords("Toad") // returns ["Toad"]
+//Write a function that takes in a string and returns an array where each element is each word that is in the string passed into the function.
+// let arrayOfWords ="Lovely Day For Coding"
+//
+// function arrayOfWords(string){
+//     return arrayOfWords.split()
+// }
+
+
+
+//TODO
+//4. Write a function that takes in a string and number and returns an object with the following properties
+// 	1. `title` which is to be set to the string passed into the function.
+// 	2. `price` which is to be set to the number passed into the function.
+// 	3. `count` which is to be set to 0.
+
+
+
+//TODO
+//5. Write a function that takes in an array of objects and returns the object from the array with the most colors in the color property.
+// ```
+const hamsters = [
     {
-        name: "Kingsford Charcoal",
-        weightInGrams: 4535
-    }, {
-        name: "Krazy Glue",
-        weightInGrams: 5
-    }, {
-        name: "NVIDIA RTX 4090",
-        weightInGrams: 2186
-    } ];
+        name: "Hamtaro",
+        heightInMM: 86,
+        fur: ['orange', 'white'],
+        gender: "male",
+        dateOfBirth: "August 6"
+    } , {
+        name: "Bijou",
+        heightInMM: 75,
+        fur: ['white'],
+        gender: "female",
+        dateOfBirth: "July 10"
+    } , {
+        name: "Oxnard",
+        heightInMM: 100,
+        fur: ['grey', 'white'],
+        gender: "male",
+        dateOfBirth: "May 3"
+    } , {
+        name: "Boss",
+        heightInMM: 120,
+        fur: ['brown', 'white'],
+        gender: "male",
+        dateOfBirth: "September 21"
+    } , {
+        name: "Snoozer",
+        heightInMM: 85,
+        fur: ['brown', 'white', "pink"],
+        gender: "male",
+        dateOfBirth: "January 14"
+    }
+];
 
-function groceryItem(array){
-    let highest = {weightInGrams: 0};
-    cart.forEach(function(carts){
-        if(cart.weightInGrams < highest.weightInGrams){
-            highest = cart;
-        }
-    });
-    return highest;
+
+function mostColors(array) {
+    for (let i = 0; i < hamsters.length; i++) {
+        console.log(hamsters[i]);
+    }
+
 }
 
-console.log(groceryItem(cart));
+
+
+
 
 
