a3c18a0b13a1e8738a1bc2579bb1b46c39fc537b
Dereck Rascon <dereckrascon76@gmail.com>
2022-11-30 19:00:46 -0700

    Finsihed mapbox-maps exercise

--- js/jQuery_intro_lec.js
+++ js/jQuery_intro_lec.js
@@ -1,7 +1,15 @@
-window.onload = function() {
-    alert( 'The page has finished loading!' );
-}
-
 $(function() {
-   alert("The DOM has finished loading!")
+    // alert( 'The DOM has finished loading!' );
+    // alert($("#intro-heading").html());
+    let listItems = $(".list-item");
+    let paragraphs = $("p");
+    $("#intro-heading").css("color", "orangered");
+    console.log(document.getElementsByClassName("list-item"));
+    console.log(listItems);
+    listItems.css("color", "blue");
+    alert(paragraphs.html());
+    paragraphs.html("Man that was a lot of nothing.");
+
+    $(".list-item, #intro-heading").css("text-decoration", "underline");
+    $("*").css("border", "1px dashed rebeccapurple");
 });
